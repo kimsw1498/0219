69ff980fa6dda0cdad717528b64931a7c64cc6ad
kimsw1498 <kimsw1498@gmail.com>
2026-02-23 14:15:19 +0900

    0223 수정

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function switchTab(e, tabId) {
     document.querySelectorAll('.content-view').forEach(v => v.classList.remove('active'));
     document.querySelectorAll('.category-btn').forEach(b => b.classList.remove('active'));
-    document.getElementById(tabId).classList.add('active');
+    
+    const target = document.getElementById(tabId);
+    if(target) target.classList.add('active');
+    
     e.currentTarget.classList.add('active');
     document.getElementById('main-scroll').scrollTop = 0;
 }
